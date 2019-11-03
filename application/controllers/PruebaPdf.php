f19c79ef362ed761f8ca361aa81de77cdb0fa642
HENRY <prez.gumiel@gmail.com>
2019-11-02 20:52:24 -0400

    'Inicio'

---
+++ application/controllers/PruebaPdf.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PruebaPdf extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+	public function crearPdfPropuestABasica() // Esta forma es una propuesta para tener una integracion como libreria
+	{
+		$this->load->library('pdf/pdfCI');
+
+		$FPDF = $this->pdfci->FPDF;
+
+		$FPDF->AddPage('P','A4',0);
+		$FPDF->SetFont('Arial','B',16);
+		$FPDF->Cell(0,0,'Hola mundo 1 FPDF desde Codeigniter',0,1,'C');
+		$FPDF->Output('paginaEnBlanco.pdf' , 'I' );    
+				
+	}
+
+	public function crearPdfPropuestaAvanzada() 
+	{
+		$this->load->library('pdf/pdfCI');
+
+		$FPDF = $this->pdfci->FPDF;
+
+		$this->pdfci->generarCabecera(); // En la cabecera estara la configuracion como colores tamaños tipos de letras como otras caracteristicas  y trabajar solamente en los datos en las funciones
+		
+		$FPDF->Cell(0,0,'Hola mundo 2 FPDF desde Codeigniter',0,1,'C');
+		$FPDF->Output('paginaEnBlanco.pdf' , 'I' );    
+				
+	}
+
+	public function crearPdfFormaDirecta()
+	{
+
+		$this->load->library('pdf/pdfCI');
+
+		$pdf = new FPDF();
+		$pdf->AddPage('P','A4',0);
+		$pdf->SetFont('Arial','B',16);
+		$pdf->Cell(0,0,'Hola mundo 3 FPDF desde Codeigniter',0,1,'C');
+		$pdf->Output('paginaEnBlanco.pdf' , 'I' );
+	}
+
+}
+
+/* End of file PruebaPdf.php */
+/* Location: ./application/controllers/PruebaPdf.php */
---
+++ application/libraries/pdf/pdfCI.php
@@ -0,0 +1,28 @@
+<?php 
+require_once APPPATH.'third_party/fpdf/fpdf.php';
+class PdfCI {
+   	//funciones que queremos implementar en Miclase.
+	public $FPDF;
+	
+
+    public function __construct()
+    {
+    	$this->FPDF = new FPDF();
+	}	
+
+	// Los siguientes metodos son para tener mas estructurado la creacion de pdf
+
+	public function generarCabecera()
+	{
+		$this->FPDF->AddPage('P','A4',0);
+		$this->FPDF->SetFont('Arial','B',16);
+	}
+
+	public function generarPieDePagina()
+	{
+
+	}
+
+}
+
+?>
